602fe093858a51a79784284678f64bf2f17bfe36
John Doe <johndoe@example.com>
2017-03-19 15:17:38 +0200

    Add date;

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const PORT = 8080;
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello world from Node.JS via Nexus 5x\n');
+  res.send('Hello world from Node.JS via Nexus 5x 19.03.17\n');
 });
 
 app.listen(PORT);
